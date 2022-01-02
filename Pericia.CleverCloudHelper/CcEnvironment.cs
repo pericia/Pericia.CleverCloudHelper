80ea8f1a031fa1995bcfb9cccd4fac11d2973828
Guillaume Lacasa <github@adhess.net>
2022-01-02 16:19:21 +0100

    Bug config effacée lorsqu'utilisé hors de Clever cloud

--- Pericia.CleverCloudHelper/CcEnvironment.cs
+++ Pericia.CleverCloudHelper/CcEnvironment.cs
@@ -22,39 +22,39 @@ namespace Pericia.CleverCloudHelper
             }
         }
 
-        public static string InstanceType => Environment.GetEnvironmentVariable("INSTANCE_TYPE") ?? "";
+        public static string? InstanceType => Environment.GetEnvironmentVariable("INSTANCE_TYPE");
 
-        public static string InstanceId => Environment.GetEnvironmentVariable("INSTANCE_ID") ?? "";
+        public static string? InstanceId => Environment.GetEnvironmentVariable("INSTANCE_ID");
 
-        public static string PrettyInstanceName => Environment.GetEnvironmentVariable("CC_PRETTY_INSTANCE_NAME") ?? "";
+        public static string? PrettyInstanceName => Environment.GetEnvironmentVariable("CC_PRETTY_INSTANCE_NAME");
 
-        public static string AppId => Environment.GetEnvironmentVariable("APP_ID") ?? "";
+        public static string? AppId => Environment.GetEnvironmentVariable("APP_ID");
 
-        public static string AppHomePath => Environment.GetEnvironmentVariable("APP_HOME") ?? "";
+        public static string? AppHomePath => Environment.GetEnvironmentVariable("APP_HOME");
 
-        public static string DeploymentId => Environment.GetEnvironmentVariable("CC_DEPLOYMENT_ID") ?? "";
+        public static string? DeploymentId => Environment.GetEnvironmentVariable("CC_DEPLOYMENT_ID");
 
-        public static string CommitId => Environment.GetEnvironmentVariable("COMMIT_ID") ?? "";
+        public static string? CommitId => Environment.GetEnvironmentVariable("COMMIT_ID");
 
         public static string[] ReverseProxyIps => Environment.GetEnvironmentVariable("CC_REVERSE_PROXY_IPS")?.Split(',') ?? new string[0];
 
 
         // .NET
 
-        public static string DotnetVersion => Environment.GetEnvironmentVariable("CC_DOTNET_VERSION") ?? "";
-        public static string DotnetProj => Environment.GetEnvironmentVariable("CC_DOTNET_PROJ") ?? "";
-        public static string DotnetTfm => Environment.GetEnvironmentVariable("CC_DOTNET_TFM") ?? "";
-        public static string DotnetProfile => Environment.GetEnvironmentVariable("CC_DOTNET_PROFILE") ?? "";
-        public static string RunCommand => Environment.GetEnvironmentVariable("CC_RUN_COMMAND") ?? "";
+        public static string? DotnetVersion => Environment.GetEnvironmentVariable("CC_DOTNET_VERSION");
+        public static string? DotnetProj => Environment.GetEnvironmentVariable("CC_DOTNET_PROJ");
+        public static string? DotnetTfm => Environment.GetEnvironmentVariable("CC_DOTNET_TFM");
+        public static string? DotnetProfile => Environment.GetEnvironmentVariable("CC_DOTNET_PROFILE");
+        public static string? RunCommand => Environment.GetEnvironmentVariable("CC_RUN_COMMAND");
 
 
         // MySQL
 
-        public static string MysqlHost => Environment.GetEnvironmentVariable("MYSQL_ADDON_HOST") ?? "";
-        public static string MysqlPort => Environment.GetEnvironmentVariable("MYSQL_ADDON_PORT") ?? "";
-        public static string MysqlDatabase => Environment.GetEnvironmentVariable("MYSQL_ADDON_DB") ?? "";
-        public static string MysqlUser => Environment.GetEnvironmentVariable("MYSQL_ADDON_USER") ?? "";
-        public static string MysqlPassword => Environment.GetEnvironmentVariable("MYSQL_ADDON_PASSWORD") ?? "";
+        public static string? MysqlHost => Environment.GetEnvironmentVariable("MYSQL_ADDON_HOST");
+        public static string? MysqlPort => Environment.GetEnvironmentVariable("MYSQL_ADDON_PORT");
+        public static string? MysqlDatabase => Environment.GetEnvironmentVariable("MYSQL_ADDON_DB");
+        public static string? MysqlUser => Environment.GetEnvironmentVariable("MYSQL_ADDON_USER");
+        public static string? MysqlPassword => Environment.GetEnvironmentVariable("MYSQL_ADDON_PASSWORD");
 
         public static string? MySqlConnectionString
         {
@@ -70,11 +70,11 @@ namespace Pericia.CleverCloudHelper
         }
 
         // PostgresSql
-        public static string PgsqlHost => Environment.GetEnvironmentVariable("POSTGRESQL_ADDON_HOST") ?? "";
-        public static string PgsqlPort => Environment.GetEnvironmentVariable("POSTGRESQL_ADDON_PORT") ?? "";
-        public static string PgsqlDatabase => Environment.GetEnvironmentVariable("POSTGRESQL_ADDON_DB") ?? "";
-        public static string PgsqlUser => Environment.GetEnvironmentVariable("POSTGRESQL_ADDON_USER") ?? "";
-        public static string PgsqlPassword => Environment.GetEnvironmentVariable("POSTGRESQL_ADDON_PASSWORD") ?? "";
+        public static string? PgsqlHost => Environment.GetEnvironmentVariable("POSTGRESQL_ADDON_HOST");
+        public static string? PgsqlPort => Environment.GetEnvironmentVariable("POSTGRESQL_ADDON_PORT");
+        public static string? PgsqlDatabase => Environment.GetEnvironmentVariable("POSTGRESQL_ADDON_DB");
+        public static string? PgsqlUser => Environment.GetEnvironmentVariable("POSTGRESQL_ADDON_USER");
+        public static string? PgsqlPassword => Environment.GetEnvironmentVariable("POSTGRESQL_ADDON_PASSWORD");
 
         public static string? PgsqlConnectionString
         {
@@ -90,31 +90,31 @@ namespace Pericia.CleverCloudHelper
         }
 
         // MongoDB
-        public static string MongodbDb => Environment.GetEnvironmentVariable("MONGODB_ADDON_DB") ?? "";
-        public static string MongodbUser => Environment.GetEnvironmentVariable("MONGODB_ADDON_USER") ?? "";
-        public static string MongodbPassword => Environment.GetEnvironmentVariable("MONGODB_ADDON_PASSWORD") ?? "";
+        public static string? MongodbDb => Environment.GetEnvironmentVariable("MONGODB_ADDON_DB");
+        public static string? MongodbUser => Environment.GetEnvironmentVariable("MONGODB_ADDON_USER");
+        public static string? MongodbPassword => Environment.GetEnvironmentVariable("MONGODB_ADDON_PASSWORD");
 
 
         // Redis
-        public static string RedisHost => Environment.GetEnvironmentVariable("REDIS_HOST") ?? "";
-        public static string RedisPort => Environment.GetEnvironmentVariable("REDIS_PORT") ?? "";
-        public static string RedisPassword => Environment.GetEnvironmentVariable("REDIS_PASSWORD") ?? "";
+        public static string? RedisHost => Environment.GetEnvironmentVariable("REDIS_HOST");
+        public static string? RedisPort => Environment.GetEnvironmentVariable("REDIS_PORT");
+        public static string? RedisPassword => Environment.GetEnvironmentVariable("REDIS_PASSWORD");
 
         // ElasticSearch
 
-        public static string ElasticsearchUri => Environment.GetEnvironmentVariable("ES_ADDON_URI") ?? "";
-        public static string ElasticsearchHost => Environment.GetEnvironmentVariable("ES_ADDON_HOST") ?? "";
-        public static string ElasticsearchUser => Environment.GetEnvironmentVariable("ES_ADDON_USER") ?? "";
-        public static string ElasticsearchPassword => Environment.GetEnvironmentVariable("ES_ADDON_PASSWORD") ?? "";
+        public static string? ElasticsearchUri => Environment.GetEnvironmentVariable("ES_ADDON_URI");
+        public static string? ElasticsearchHost => Environment.GetEnvironmentVariable("ES_ADDON_HOST");
+        public static string? ElasticsearchUser => Environment.GetEnvironmentVariable("ES_ADDON_USER");
+        public static string? ElasticsearchPassword => Environment.GetEnvironmentVariable("ES_ADDON_PASSWORD");
 
-        public static string KibanaHost => Environment.GetEnvironmentVariable("ES_ADDON_KIBANA_HOST") ?? "";
-        public static string KibanaUser => Environment.GetEnvironmentVariable("ES_ADDON_KIBANA_USER") ?? "";
-        public static string KibanaPassword => Environment.GetEnvironmentVariable("ES_ADDON_KIBANA_PASSWORD") ?? "";
+        public static string? KibanaHost => Environment.GetEnvironmentVariable("ES_ADDON_KIBANA_HOST");
+        public static string? KibanaUser => Environment.GetEnvironmentVariable("ES_ADDON_KIBANA_USER");
+        public static string? KibanaPassword => Environment.GetEnvironmentVariable("ES_ADDON_KIBANA_PASSWORD");
 
-        public static string ApmHost => Environment.GetEnvironmentVariable("ES_ADDON_APM_HOST") ?? "";
-        public static string ApmAuthToken => Environment.GetEnvironmentVariable("ES_ADDON_APM_AUTH_TOKEN") ?? "";
-        public static string ApmUser => Environment.GetEnvironmentVariable("ES_ADDON_APM_USER") ?? "";
-        public static string ApmPassword => Environment.GetEnvironmentVariable("ES_ADDON_APM_PASSWORD") ?? "";
+        public static string? ApmHost => Environment.GetEnvironmentVariable("ES_ADDON_APM_HOST");
+        public static string? ApmAuthToken => Environment.GetEnvironmentVariable("ES_ADDON_APM_AUTH_TOKEN");
+        public static string? ApmUser => Environment.GetEnvironmentVariable("ES_ADDON_APM_USER");
+        public static string? ApmPassword => Environment.GetEnvironmentVariable("ES_ADDON_APM_PASSWORD");
 
     }
 
